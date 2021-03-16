0ba7757ef52b2ec420838663ad00740d46005caf
YuanCome <tanheyuan@outlook.com>
2021-03-16 13:48:01 +0800

    修改点：1、整合jolokia，注册UserMBean。

--- java-training-camp/chapter-one/user-platform/user-web/src/main/java/org/geektimes/projects/user/web/listener/ComponentContextInitializerListener.java
+++ java-training-camp/chapter-one/user-platform/user-web/src/main/java/org/geektimes/projects/user/web/listener/ComponentContextInitializerListener.java
@@ -2,10 +2,18 @@ package org.geektimes.projects.user.web.listener;
 
 
 import org.geektimes.Context.ComponentContext;
+import org.geektimes.projects.user.domain.User;
+import org.geektimes.projects.user.jmx.MBeanManager;
+import org.geektimes.projects.user.management.UserManager;
 
+import javax.management.MBeanServer;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import java.lang.management.ManagementFactory;
+import java.util.logging.Logger;
 
 /**
  * {@link ComponentContext} 初始化器
@@ -13,11 +21,18 @@ import javax.servlet.ServletContextListener;
  */
 public class ComponentContextInitializerListener implements ServletContextListener {
 
+    Logger logger = Logger.getLogger("org.geektimes...ComponentContextInitializerListener");
+
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         ServletContext servletContext = sce.getServletContext();
         ComponentContext context = new ComponentContext();
         context.init(servletContext);
+        try {
+            MBeanManager.registerMBean("org.geektimes.projects.user.management:type=User", new User());
+        } catch (Exception e) {
+            logger.severe("注册 UserMBean 失败，异常信息：" + e);
+        }
     }
 
     @Override
