5ffaa97fbb5d589b6d0c8cf29f40ea692f7f024c
YuanCome <tanheyuan@outlook.com>
2021-03-31 14:38:05 +0800

    修改点：1、完成作业2，并填写README文件描述，缺陷：Post请求接口需要设置相应的返回参数及内容与其他人不一样，未找到原因

--- java-training-camp/chapter-one/user-platform/my-rest-client/src/test/java/org/geektimes/rest/demo/RestClientDemo.java
+++ java-training-camp/chapter-one/user-platform/my-rest-client/src/test/java/org/geektimes/rest/demo/RestClientDemo.java
@@ -2,20 +2,27 @@ package org.geektimes.rest.demo;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Response;
 
 public class RestClientDemo {
 
     public static void main(String[] args) {
         Client client = ClientBuilder.newClient();
-        Response response = client
-                .target("http://127.0.0.1:8080/hello/world")      // WebTarget
+        //Response responseGet = client
+        //        .target("http://127.0.0.1:8080/hello/world")      // WebTarget
+        //        .request() // Invocation.Builder
+        //        .get();                                     //  Response
+        //String contentGet = responseGet.readEntity(String.class);
+        //System.out.println("Content Get : " + contentGet);
+
+        Entity<String> stringEntity = Entity.text("YuanCome");
+        Response responsePost = client
+                .target("http://127.0.0.1:8080/hello/test")      // WebTarget
                 .request() // Invocation.Builder
-                .get();                                     //  Response
-
-        String content = response.readEntity(String.class);
-
-        System.out.println(content);
-
+                .header("Content-Type", "text/plain")
+                .post(stringEntity);                              //  Response
+        String contentPost = responsePost.readEntity(String.class);
+        System.out.println("Content Post : " + contentPost);
     }
 }
