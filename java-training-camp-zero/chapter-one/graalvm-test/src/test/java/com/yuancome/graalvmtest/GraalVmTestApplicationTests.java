e2236c39885f79361e978aa59374aa0b71cca63a
YuanCome <tanheyuan@outlook.com>
2021-07-05 20:35:20 +0800

    修改点：1、优化.gitignore文件省略文件

---
+++ java-training-camp-zero/chapter-one/graalvm-test/src/test/java/com/yuancome/graalvmtest/GraalVmTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuancome.graalvmtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraalVmTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
