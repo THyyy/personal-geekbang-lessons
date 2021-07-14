8940e3c7b2983520619dbe234d84585c9113a74b
YuanCome <tanheyuan@outlook.com>
2021-07-14 20:33:57 +0800

    修改点：1、同步上课代码；2、完成第一期第二周作业。

---
+++ java-training-camp-one/middleware-frameworks/my-configuration/src/main/java/org/geektimes/configuration/microprofile/config/source/servlet/ServletRequestConfigSource.java
@@ -0,0 +1,66 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.geektimes.configuration.microprofile.config.source.servlet;
+
+import org.eclipse.microprofile.config.spi.ConfigSource;
+
+import javax.servlet.ServletRequest;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * {@link ServletRequest} {@link ConfigSource}
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @since 1.0.0
+ */
+public class ServletRequestConfigSource implements ConfigSource {
+
+    private final ServletRequest servletRequest;
+
+    private final String name;
+
+    public ServletRequestConfigSource(String name, ServletRequest servletRequest) {
+        this.servletRequest = servletRequest;
+        this.name = name;
+    }
+
+    @Override
+    public Set<String> getPropertyNames() {
+        return servletRequest.getParameterMap().keySet();
+    }
+
+    @Override
+    public String getValue(String propertyName) {
+        return servletRequest.getParameter(propertyName);
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public Map<String, String> getProperties() {
+        return ConfigSource.super.getProperties();
+    }
+
+    @Override
+    public int getOrdinal() {
+        return ConfigSource.super.getOrdinal();
+    }
+}
