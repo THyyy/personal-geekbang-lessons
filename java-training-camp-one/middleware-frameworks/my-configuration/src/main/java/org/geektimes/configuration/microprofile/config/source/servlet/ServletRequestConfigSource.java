07af14c45c36855f9a728ec4dfa212a980fc96ad
YuanCome <tanheyuan@outlook.com>
2021-07-14 20:48:09 +0800

    修改点：2、完成第一期第二周作业。

--- java-training-camp-one/middleware-frameworks/my-configuration/src/main/java/org/geektimes/configuration/microprofile/config/source/servlet/ServletRequestConfigSource.java
+++ java-training-camp-one/middleware-frameworks/my-configuration/src/main/java/org/geektimes/configuration/microprofile/config/source/servlet/ServletRequestConfigSource.java
@@ -19,6 +19,8 @@ package org.geektimes.configuration.microprofile.config.source.servlet;
 import org.eclipse.microprofile.config.spi.ConfigSource;
 
 import javax.servlet.ServletRequest;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * {@link ServletRequest} {@link ConfigSource}
@@ -26,5 +28,39 @@ import javax.servlet.ServletRequest;
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
  * @since 1.0.0
  */
-public class ServletRequestConfigSource {
+public class ServletRequestConfigSource implements ConfigSource {
+
+    private final ServletRequest servletRequest;
+
+    private final String name;
+
+    public ServletRequestConfigSource(String name, ServletRequest servletRequest) {
+        this.servletRequest = servletRequest;
+        this.name = name;
+    }
+
+    @Override
+    public Set<String> getPropertyNames() {
+        return servletRequest.getParameterMap().keySet();
+    }
+
+    @Override
+    public String getValue(String propertyName) {
+        return servletRequest.getParameter(propertyName);
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public Map<String, String> getProperties() {
+        return ConfigSource.super.getProperties();
+    }
+
+    @Override
+    public int getOrdinal() {
+        return ConfigSource.super.getOrdinal();
+    }
 }
